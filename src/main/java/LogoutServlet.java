fd3fb9c915f88ad9ac7c34e14ad7cbb0e809dc22
Petr Široký <psiroky@redhat.com>
2017-02-03 17:35:01 +0100

    Initial commit

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+        request.logout();
+        HttpSession session = request.getSession(false);
+        session.invalidate();
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Logged out!</h1>");
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+
+}
